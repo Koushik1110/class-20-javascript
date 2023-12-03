fc9aca48cd8c1c315ae0e43f9bb2a082055867da
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2023-12-03 21:59:23 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+//dom ---> document object model.
+// it is not a property of javascript it is provided by the browser
+
+//api---> application programming interface
+
+//dom shows a converted structure of the document. 
